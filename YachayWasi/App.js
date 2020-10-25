49c295e14c4df29841a63303989c3ba878883ee2
Gustavo Salazar <gustersalazar@gmail.com>
2020-10-24 21:30:53 -0400

    login implemented without functionality

--- YachayWasi/App.js
+++ YachayWasi/App.js
@@ -2,10 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import UserLogin from './app/views/user_login.js';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your apps!</Text>
+      <UserLogin></UserLogin>
       <StatusBar style="auto" />
     </View>
   );
